c8a7a39f9b2ce02fffb5249f002d78bca00d9d9e
IronXu-Dylan <1033375448@qq.com>
2018-10-31 21:50:40 +0800

    equation.c rm commit

--- equation.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-main() {
-	double a,b,c,d,e,f,x,y;
-	scanf("%lf%lf%lf%lf%lf%lf",&a,&b,&c,&d,&e,&f);
-	x=(b*f-c*e)/(b*d-a*e);
-	y=(c*d-a*f)/(b*d-a*e);
-    printf("x=%lf,y=%lf",x,y);
-    return 0;
-} 
